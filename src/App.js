1c2cb9ff503a4ee97f80aa7697da3b582d76affa
Emmanuel Amoo <hahyordeji@gmail.com>
2023-03-09 12:08:04 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/Form.js 	new file:   src/TodoList.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Form from "./Form";
+import './index.css'
+import TodoList from "./TodoList";
+
 
 function App() {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [todos, setTodos] = useState(initialState);
+  const [input, setInput] = useState("");
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-wrapper">
+        <header className="app-header">
+          <h1>To-Do List</h1>
+        </header>
+        
+        <Form input={input}
+          setInput = {setInput}
+          todos={todos}
+          setTodos ={setTodos}
+          editTodo={editTodo}
+          setEditTodo ={setEditTodo}
+        />
+        <TodoList
+          // input={input}
+          // setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          setEditTodo={setEditTodo}
+        />
+        
+        
+      </div>
+      <div>
+      
+      </div>
+      
     </div>
   );
 }
